1cfa5702c457798bfda25911bbd6cd66b614676f
Julia Khayrulina <julia748@mail.ru>
2021-08-05 16:00:56 +0300

    The program runs:
    - If the user number is larger, the bot outputs 'Guessed number is less' and suggests introducing a new variant
    - If the user number is smaller, the bot outputs 'The guessed number is greater' and suggests introducing a new variant
    - if the user has entered not a number, he displays the message 'Enter a number!' and offers to enter a new option
    - if the user clicks 'Cancel', the game ends and the message 'Game is over' is displayed
    - If the custom number is equal to the guess, then the game ends and displays the message 'Congratulations, you guessed!!!'

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 'use strict';
+
+const getNumber = keepsNum(60);
+
+function keepsNum(number) {
+  function giveNum() {
+    return number;
+  }
+  return giveNum;
+}
+
+function guessNumber() {
+  let value = prompt('Угадай число от 1 до 100', '');
+
+  if (isNumber(value)) {
+    if (checkRange(+value)) {
+      if (+value > getNumber()) {
+        alert('Загаданное число меньше');
+        guessNumber();
+      } else if (+value < getNumber()) {
+        alert('Загаданное число больше');
+        guessNumber();
+      } else {
+        alert('Поздравляю, Вы угадали!!!');
+      }
+    } else {
+      guessNumber();
+    }
+  } else if (value === null) {
+    alert('Игра окончена!');
+  } else {
+    alert('Введи число!');
+    guessNumber();
+  }
+}
+
+guessNumber();
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function checkRange(val) {
+  if (val < 1) {
+    alert('Вы ввели число меньше 1');
+  } else if (val > 101) {
+    alert('Вы ввели число больше 100');
+  } else {
+    return true;
+  }
+}
